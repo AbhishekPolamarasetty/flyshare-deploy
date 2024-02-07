9a431a402d54fb2d08dec0f0af1517c5fef09ce6
AbhishekPolamarasetty <pashek321@gmail.com>
2024-02-07 19:45:56 +0530

    added the email host and password in settings.py

--- Flyshare/settings.py
+++ Flyshare/settings.py
@@ -157,8 +157,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'balisettytharun2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'mqoaargctoidqssj'
+EMAIL_HOST_USER = 'flyshare.services@gmail.com'
+EMAIL_HOST_PASSWORD = 'qxwbhihygzrjuqva'
 
 # AUTHENTICATION_BACKENDS = [
 #     'django.contrib.auth.backends.ModelBackend',
